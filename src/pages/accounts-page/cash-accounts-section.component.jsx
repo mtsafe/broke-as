7f95d83d75aa7a8f2acd7112d06a1d9e292e45aa
Albir Tarsha <a.tarsha@yahoo.com>
2020-09-23 14:09:20 -0500

    Changed CashAccountsSection to class
    Changed the component CashAccountsSection to a class so that it can hold state.

--- src/pages/accounts-page/cash-accounts-section.component.jsx
+++ src/pages/accounts-page/cash-accounts-section.component.jsx
@@ -6,46 +6,57 @@ import AssessmentModal from "./assessment-modal.component";
 import FormContainer from "./form-container.component";
 import Table from "../../components/table/table.component";
 
-const CashAccountsSection = (props) => (
-  <section id="cash-accounts">
-    {/* <!-- Cash Accounts Section --> */}
-    <h2 className="white-text">Cash Accounts</h2>
-    <FormContainer type="cash" />
-    <AssessmentModal type="cash" />{" "}
-    <header id="cash-table-header">
-      <span id="cash-form-instructions" className="lead">
-        Where is your cash and liquids accounts such as checking accounts?
-      </span>
-      <h3 className="center-align float-right">
-        Cash on Hand: $<span id="cash-total-amount">0.00</span>
-      </h3>
-      <button
-        id="cash-badge"
-        data-toggle="modal"
-        data-target="#cash-assessment-modal"
-        className="badge badge-pill badge-primary float-right"
-      >
-        <small>Set Warning Limits</small>
-      </button>
-    </header>
-    <Table headings={("cash", ["Where", "Amount", "Recorded"])} />
-    <footer id="cash-table-footer">
-      <button
-        id="cash-add-tr-btn"
-        className="btn btn-outline-success float-left"
-      >
-        <i className="fa fa-plus"></i> Add a Money Row to Table
-      </button>
-      <button
-        id="cash-clear-all-btn"
-        type="button"
-        className="btn btn-outline-danger float-right"
-      >
-        Purge Cash Accounts
-      </button>
-    </footer>
-    {/* <!-- End of Cash Accounts Section --> */}
-  </section>
-);
+class CashAccountsSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { edit: "addCash" };
+    // edit states are: "off", "addCash", "modifyCash"
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  render() {
+    return (
+      <section id="cash-accounts">
+        {/* <!-- Cash Accounts Section --> */}
+        <h2 className="white-text">Cash Accounts</h2>
+        <FormContainer type="cash" />
+        <AssessmentModal type="cash" />{" "}
+        <header id="cash-table-header">
+          <span id="cash-form-instructions" className="lead">
+            Where is your cash and liquids accounts such as checking accounts?
+          </span>
+          <h3 className="center-align float-right">
+            Cash on Hand: $<span id="cash-total-amount">0.00</span>
+          </h3>
+          <button
+            id="cash-badge"
+            data-toggle="modal"
+            data-target="#cash-assessment-modal"
+            className="badge badge-pill badge-primary float-right"
+          >
+            <small>Set Warning Limits</small>
+          </button>
+        </header>
+        <Table headings={("cash", ["Where", "Amount", "Recorded"])} />
+        <footer id="cash-table-footer">
+          <button
+            id="cash-add-tr-btn"
+            className="btn btn-outline-success float-left"
+          >
+            <i className="fa fa-plus"></i> Add a Money Row to Table
+          </button>
+          <button
+            id="cash-clear-all-btn"
+            type="button"
+            className="btn btn-outline-danger float-right"
+          >
+            Purge Cash Accounts
+          </button>
+        </footer>
+        {/* <!-- End of Cash Accounts Section --> */}
+      </section>
+    );
+  }
+}
 
 export default CashAccountsSection;
