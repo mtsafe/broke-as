dbd25ed61e0431d8cddca42f287ebe7ce4779cf1
Albir Tarsha <a.tarsha@yahoo.com>
2021-04-29 02:59:33 -0500

    fixed test error
    Error:
    Invariant failed: You should not use <Switch> outside a <Router>
    Fixed by wrapping with MemoryRouter

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
 import { render } from '@testing-library/react'
 import App from './App'
 
 test('renders "We are sorry." span', () => {
-  const { getByText } = render(<App />)
+  const { getByText } = render(<App />, { wrapper: MemoryRouter })
   const linkElement = getByText(/"We are sorry."/i)
   expect(linkElement).toBeInTheDocument()
 })
