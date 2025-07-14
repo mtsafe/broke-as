9077becc433cd4ef1134e8b630ba7babb9fe4715
Albir Tarsha <a.tarsha@yahoo.com>
2025-07-14 01:43:05 -0500

    Update App.test.js
    Fix for:
        `ReactDOMTestUtils.act` is deprecated in favor of `React.act`. Import `act` from `react` instead of `react-dom/test-utils`. See https://react.dev/warnings/react-dom-test-utils for more info.
           5 |
           6 | test('renders "We are sorry." span', () => {
        >  7 |   const { getByText } = render(<App />, { wrapper: MemoryRouter })
             |                               ^

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import { MemoryRouter } from 'react-router-dom'
-import { render } from '@testing-library/react'
-import App from './App'
+import React from "react"
+import { MemoryRouter } from "react-router-dom"
+//import { render } from '@testing-library/react'
+import { render } from "react"
+import App from "./App"
 
 test('renders "We are sorry." span', () => {
   const { getByText } = render(<App />, { wrapper: MemoryRouter })
