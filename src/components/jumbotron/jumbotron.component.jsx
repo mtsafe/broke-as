a48427f47734503d4c775ea00bfa06f17c2a8573
Albir Tarsha <a.tarsha@yahoo.com>
2025-07-13 21:41:59 -0500

    got cypress working for component testing
    Upgraded cypress to v14.5.1
    Added webpack
    Added sass

--- src/components/jumbotron/jumbotron.component.jsx
+++ src/components/jumbotron/jumbotron.component.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import isNonEmptyString from "../../utils/utils";
+import React from "react"
+import isNonEmptyString from "../../utils/utils"
 
-import "./jumbotron.styles.scss";
-import BtnLearnMore from "../../components/btn-learn-more/btn-learn-more.component";
+import "./jumbotron.styles.scss"
+import BtnLearnMore from "../../components/btn-learn-more/btn-learn-more.component"
 
-const Jumbotron = (props) => {
-  const isGoodContent = isNonEmptyString(props.content);
+const Jumbotron = ({ title, lead, content }) => {
+  const isGoodContent = isNonEmptyString(content)
 
   return (
     <div className="jumbotron">
-      <h1 className="display-3">{props.title}</h1>
-      <p className="lead">{props.lead}</p>
+      <h1 className="display-3">{title}</h1>
+      <p className="lead">{lead}</p>
       {isGoodContent && (
         <React.Fragment>
           <hr className="my-4" />
-          <p>{props.content}</p>
+          <p>{content}</p>
         </React.Fragment>
       )}
       <BtnLearnMore />
     </div>
-  );
-};
+  )
+}
 
-export default Jumbotron;
+export default Jumbotron
--- src/components/jumbotron/jumbotron.cy.jsx
+++ src/components/jumbotron/jumbotron.cy.jsx
@@ -1,9 +1,39 @@
-import * as React from 'react'
-import { mount } from '@cypress/react'
-import Jumbotron from './jumbotron.component'
-import '../../assets/sketchy/bootstrap.min.css'
+import * as React from "react"
+import Jumbotron from "./jumbotron.component"
+import "../../assets/sketchy/bootstrap.min.css"
 
-it('Jumbotron', () => {
-  mount(<Jumbotron>Foo</Jumbotron>)
-  cy.get('.jumbotron')
+describe("Test Jumbotrom", () => {
+  it("Jumbotron mounts", () => {
+    cy.mount(<Jumbotron />)
+    cy.get(".jumbotron")
+  })
+  it("Jumbotron 2 mounts: full data", () => {
+    cy.mount(
+      <Jumbotron
+        title="Jumbotron Title"
+        lead="This is the Jumbotron lead text."
+        content="This is the Jumbotron content."
+      />
+    )
+    cy.get(".jumbotron")
+    cy.get(".jumbotron h1.display-3")
+    cy.get(".jumbotron p.lead")
+    cy.get(".jumbotron hr.my-4")
+    cy.get(".jumbotron p")
+    cy.get(".jumbotron p.lead a")
+  })
+  it("Jumbotron 3 mounts: no content", () => {
+    cy.mount(
+      <Jumbotron
+        title="Jumbotron Title"
+        lead="This is the Jumbotron lead text."
+        content=""
+      />
+    )
+    cy.get(".jumbotron")
+    cy.get(".jumbotron h1.display-3")
+    cy.get(".jumbotron p.lead")
+    cy.get(".jumbotron hr.my-4").should("not.exist")
+    cy.get(".jumbotron p.lead a")
+  })
 })
