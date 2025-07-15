2992d09c89fb4970ec0cbf212b938c6a26b921b9
Albir Tarsha <a.tarsha@yahoo.com>
2025-07-15 09:29:33 -0500

    Fixed App failed due to React 18 change
    React 18 introduced a new root API, replacing ReactDOM.render with ReactDOM.createRoot.
    If using React 18 or later, modify your index.js file (or equivalent) to use createRoot and then call render on the created root.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 
 // Bootstrap 4.5.2 : Bootswatch : Sketchy
-import "./assets/sketchy/bootstrap.min.css";
-import "./assets/index.scss";
-import App from "./App";
+import "./assets/sketchy/bootstrap.min.css"
+import "./assets/index.scss"
+import App from "./App"
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
-);
+)
