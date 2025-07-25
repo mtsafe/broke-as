a80772d70bb1a37ed8ef5cdf1da428cd62165877
Albir Tarsha <a.tarsha@yahoo.com>
2025-07-24 21:31:55 -0500

    Fixed eslint error
    [eslint]
    src/App.js
      Line 1:17:  'Component' is defined but never used  no-unused-vars

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react"
+// import React, { Component } from "react"
+import React from "react"
 import { Route, Switch } from "react-router-dom"
 import "./assets/App.scss"
 
